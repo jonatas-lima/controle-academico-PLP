146687dcca80248669f5b6c0f673f7dd0a1a18e6
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2021-08-17 20:42:50 -0300

    change: mudança de nome de variavel

--- funcional/Usuario.hs
+++ funcional/Usuario.hs
@@ -6,13 +6,18 @@ data Usuario = Usuario
     role :: String
   }
 
+-- / Verifica se um usuário e senha está cadastrado no sistema e retorna (Se está autenticado, seu 'role')
 autentica :: String -> String -> [Usuario] -> (Bool, String)
-autentica usuario pwd usuariosDisponiveis =
-  ((usuario, pwd) `elem` usuariosSenhas usuariosDisponiveis, findRole usuario pwd usuariosDisponiveis)
+autentica usuario senha usuariosDisponiveis =
+  (parUsuarioSenha `elem` usuariosESenhas, role)
+  where
+    usuariosESenhas = usuariosSenhas usuariosDisponiveis
+    parUsuarioSenha = (usuario, senha)
+    role = findRole usuario senha usuariosDisponiveis
 
 usuariosSenhas :: [Usuario] -> [(String, String)]
 usuariosSenhas = map (\u -> (nickname u, senha u))
 
 findRole :: String -> String -> [Usuario] -> String
 findRole _ _ [] = ""
-findRole nick pwd (u : us) = if nick == nickname u && pwd == senha u then role u else findRole nick pwd us
+findRole nick password (u : us) = if nick == nickname u && password == senha u then role u else findRole nick password us
